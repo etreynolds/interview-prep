e7f1ff0d47a3270be875cb87ed8eabc0e5ad7565
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-11-07 16:36:04 -0700

    complete exercise

--- prob-freq-count-pointers/is-subsequence.js
+++ prob-freq-count-pointers/is-subsequence.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    let str1idx = 0;
+    let str2idx = 0;
+
+    if (!str1) return true;
+    while (str2idx < str2.length) {
+        if (str2[str2idx] === str1[str1idx]) {
+            str1idx += 1;
+        }
+        if (str1idx === str1.length) return true;
+        str2idx += 1;
+    }
+    return false;
 }
