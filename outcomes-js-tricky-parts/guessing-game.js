de7d559185c192f84892fef0cca9289763826838
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-11-09 19:39:37 -0700

    finish exercises

--- outcomes-js-tricky-parts/guessing-game.js
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
+    const answer = Math.floor(Math.random() * 100);
+    let gameOver = false;
+    let numGuesses = 0;
 
+    return function guess(num) {
+        if (gameOver) return "Game is over. You already won!";
+        numGuesses++;
+        if (num === answer) {
+            gameOver = true;
+            const guess = numGuesses === 1 ? "guess" : "guesses";
+            return `You win! You found ${num} in ${numGuesses} ${guess}.`;
+        }
+        if (num < answer) return `${num} is too low!`;
+        if (num > answer) return `${num} is too high!`;
+    };
 }
 
 module.exports = { guessingGame };
